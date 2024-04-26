9426e6aa570562381be5c1ffa7b4cfc91282aa93
Henri Aïdasso <ahenrij@gmail.com>
2024-04-25 23:41:59 -0400

    fix: footer linkedin added

--- src/components/common/TheFooter.vue
+++ src/components/common/TheFooter.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="relative bottom-0 bg-stone-200 text-black py-10 px-16">
         <div class="flex">
-            <a href="https://github.com/andresoro" target="_blank" class="mr-4"><img src="@/assets/images/github.svg" class="h-10 w-10" alt="GitHub" /></a>
+            <a href="https://github.com/andre-soro" target="_blank" class="mr-4"><img src="@/assets/images/github.svg" class="h-10 w-10" alt="GitHub" /></a>
+            <a href="https://www.linkedin.com/in/andresoro" target="_blank" class="mx-4"><img src="@/assets/images/linkedin.svg" class="h-10 w-10" alt="LinkedIn" /></a>
             <a href="mailto:soro2@hotmail.fr" target="_blank" class="mx-4"><img src="@/assets/images/email.svg" class="h-10 w-10" alt="Email" /></a>
         </div>
         <p class="mt-6">© {{ (new Date()).getFullYear() }} André Soro, PhD | All rights reserved.</p>
-        <p class="mt-2 text-zinc-400">Website maintained by <a href="https://henriaidasso.me" target="_blank" class="underline">Henri Aïdasso</a></p>
+        <p class="mt-2 text-zinc-400 text-sm">Website maintained by <a href="https://henriaidasso.me" target="_blank" class="underline">Henri Aïdasso</a></p>
     </div>
 </template>
